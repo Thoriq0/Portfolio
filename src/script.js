fc3455ea776fc57698247090ee2d6ccc4d95c3d1
Thoriq0 <thoriq.ahmad1301@gmail.com>
2023-11-28 20:54:31 +0700

    add section 3,4,5,6

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+//! Navbar
 document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.querySelector('.navbar');
 
@@ -8,6 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             navbar.classList.add('transparant');
             navbar.classList.remove('colored');
+        }
+    });
+});
+
+
+//! Side Bar
+document.addEventListener('DOMContentLoaded', function () {
+    var hamburgerIcon = document.querySelector('.right');
+    var sidebar = document.querySelector('.sidebar');
+
+    // Toggle sidebar on hamburger icon click
+    hamburgerIcon.addEventListener('click', function () {
+        sidebar.classList.toggle('hidden');
+    });
+
+    // Close sidebar on clicking outside the sidebar
+    document.addEventListener('click', function (e) {
+        if (!sidebar.contains(e.target) && !hamburgerIcon.contains(e.target)) {
+            sidebar.classList.add('hidden');
         }
     });
 });
